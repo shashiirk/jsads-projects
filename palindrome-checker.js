eadea7d30c029bc02c1a8a28e03c26c00452e97b
Shashikanth Reddy <kolanshashii@gmail.com>
2021-02-05 20:01:25 +0530

    added comments to first project

--- palindrome-checker.js
+++ palindrome-checker.js
@@ -1,7 +1,11 @@
+// Returns a boolean value
 function palindrome(str) {
-  str = [...str.toLowerCase()].filter(value => value.match(/^[a-z0-9]$/)).join('');
+  // Remove all non-alphanumeric characters (punctuation, spaces and symbols) and turn everything into the same case (lower or upper case)
+  str = [...str.toLowerCase()]
+    .filter((value) => value.match(/^[a-z0-9]$/))
+    .join('');
+  // Reverse the string
   let reversedStr = [...str].reverse().join('');
+  // Check whether they are equal
   return reversedStr === str;
 }
-
-palindrome("eye");
